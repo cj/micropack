f6702899207932577d0abe4b467e18fb0fe0133e
cj <cjlazell@gmail.com>
2017-01-16 14:29:39 -0600

    tests

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
-import { send } from 'micro'
+import { send }             from 'micro'
+import { resolve }          from 'path'
+import { createReadStream } from 'fs'
+// import qs                   from 'querystring'
+import url                  from 'url'
+import webpack              from 'webpack'
+import webpackConfigFunc    from '../webpack.config.babel'
+
+const webpackConfig  = webpackConfigFunc({})
+const compiler       = webpack(webpackConfig)
+const webpackHotPath = '/__webpack_hmr'
+
+const webpackDev = require('webpack-dev-middleware')(compiler, {
+  noInfo: true, publicPath: webpackConfig.output.publicPath, quiet: true
+})
+
+const webpackHot = require('webpack-hot-middleware')(compiler, {
+  log: () => {}, path: webpackHotPath, heartbeat: 10 * 1000
+})
 
 export default async function run (req, res) {
-  send(res, 200, 'Ready!')
+  const { path } = url.parse(req.url)
+  const next     = () => { res.end() }
+
+  switch (path) {
+    case '/':
+      res.setHeader('Content-Type', 'text/html')
+      return send(res, 200, createReadStream(resolve('src/index.html')))
+    case webpackHotPath:
+      return webpackHot(req, res, next)
+    default:
+      return webpackDev(req, res, next)
+  }
 }
